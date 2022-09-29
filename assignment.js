c10de007e2bb849048f557a7bdb2b10be06c949e
rahulrajput83 <rahulrajput21112@gmail.com>
2022-09-29 20:16:19 +0530

    cleanText

--- assignment.js
+++ assignment.js
@@ -1,19 +1,17 @@
 /* 1.a */
-
-
 function countWords(paragraph, first, second) {
     let split = paragraph.split(/[\s.]+/)
     let countFirst = 0;
     let countSecond = 0;
     for (let i = 0; i < split.length; i++) {
-        if(split[i] === first) {
+        if (split[i] === first) {
             countFirst++;
         }
-        else if(split[i] === second) {
+        else if (split[i] === second) {
             countSecond++;
         }
     }
-    if(countFirst > countSecond) {
+    if (countFirst > countSecond) {
         return first
     }
     else {
@@ -22,9 +20,16 @@ function countWords(paragraph, first, second) {
 }
 
 const paragraph = 'I love teaching. If you do not love teaching what else can you love. I love JavaScript if you do not love something which can give life to your application what else can you love.';
-console.log(countWords(paragraph,'love', 'you'));
+console.log(countWords(paragraph, 'love', 'you'));
 
 
+/* 1.b */
+function cleanText(text) {
+    const replace = text.replace(/[`~!@#$%^&*()_|+\-=;:'",<>\{\}\[\]\\\/]/gi, '')
+    return replace
+}
+const sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`;
+console.log(cleanText(sentence));
 
 /* 2c */
 function reverseArray(arr) {
