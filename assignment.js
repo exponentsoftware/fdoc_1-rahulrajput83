21c4755d67cf87bc45aae8d00b8b168d99c93c95
rahulrajput83 <rahulrajput21112@gmail.com>
2022-09-29 20:22:37 +0530

    wordCount

--- assignment.js
+++ assignment.js
@@ -31,6 +31,24 @@ function cleanText(text) {
 const sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`;
 console.log(cleanText(sentence));
 
+/* 1.c */
+
+function wordCount(text) {
+    const wordText = text.split(" ");
+    let count = 0;
+    for (let i = 0; i < wordText.length; i++) {
+        if (wordText[i].length > 1) {
+            count++;
+        }
+    }
+    return count;
+}
+
+const string =  `I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?`
+console.log(wordCount(string));
+
+
+
 /* 2c */
 function reverseArray(arr) {
     return arr.reverse();
